954efc9cd1bf26802b0a0454699b13ab16b07102
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2024-09-27 11:06:29 +0300

    Update breadcrumbs.js

--- src/theme/breadcrumbs.js
+++ src/theme/breadcrumbs.js
@@ -7,58 +7,33 @@ const MAP = {
     javascript: "JavaScript"
 };
 
-const KEYS = Object.keys(MAP);
-
-function getNameFromTitle() {
-    let index = document.title.indexOf('-'); // Look for hyphen
-    if (index !== -1) {
-        return document.title.substring(0, index).trim(); 
-    } else {
-        index = document.title.indexOf('- '); // Look for hyphen and space
-        if (index !== -1) {
-            return document.title.substring(0, index).trim(); 
-        } else {
-            return document.title.trim(); // If no hyphen, use the whole title
-        }
-    }
-}
-
-const domain = location.origin; 
-let path = location.pathname; 
+const domain = location.origin;
+let path = location.pathname;
 
 let nightlyPath = '';
 if (path.includes('/nightly/')) {
     nightlyPath = '/nightly/';
-    path = path.replace('/nightly/', ''); 
+    path = path.replace('/nightly/', '');
 }
 
 if (path.endsWith(".html")) {
     path = path.substring(0, path.length - 5);
 }
 
-document.write(`<a href="${domain}${nightlyPath}">Home</a>`); 
+document.write(`<a href="${domain}${nightlyPath}">Home</a>`);
 
-const segments = path.split('/').filter(segment => segment !== ''); 
-segments.forEach((segment, i) => {
-    if (MAP.hasOwnProperty(segment.toLocaleLowerCase())) { 
+const segments = path.split('/').filter(segment => segment !== '');
+// Remove the last segment, which is the filename
+const segmentsWithoutFilename = segments.slice(0, -1);
+
+segmentsWithoutFilename.forEach(segment => {
+    if (MAP.hasOwnProperty(segment.toLocaleLowerCase())) {
         let name = MAP[segment.toLocaleLowerCase()];
-        let link = "introduction.html"; 
+        let link = "introduction.html";
         document.write(`<div><a href="${domain}${nightlyPath}${link}">${name}</a></div>`);
-    } else if (i === segments.length - 1) {
-        let name = getNameFromTitle();
-        let parentName = i > 0 ? MAP[segments[i - 1].toLocaleLowerCase()] : ""; 
-        if (parentName) {
-            let parentLink = "introduction.html";
-            document.write(`<div><a href="${domain}${nightlyPath}${parentLink}">${parentName}</a></div>`);
-        }
-        document.write(`<div><a href="${domain}${nightlyPath}${segment}.html">${name}</a></div>`);
-    } else {
-        let name = segment;
-        let parentName = i > 0 ? MAP[segments[i - 1].toLocaleLowerCase()] : ""; 
-        if (parentName) {
-            let parentLink = "introduction.html";
-            document.write(`<div><a href="${domain}${nightlyPath}${parentLink}">${parentName}</a></div>`);
-        }
-        document.write(`<div><a href="${domain}${nightlyPath}${segment}.html">${name}</a></div>`);
     }
 });
+
+// Add the last segment as the filename
+const filename = segments.slice(-1)[0];
+document.write(`<div><a href="${domain}${nightlyPath}${filename}.html">${filename}</a></div>`);
