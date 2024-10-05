0bc3e7e7828c8c8f42d366f03849a68131f1c9d6
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2024-10-05 10:53:41 +0300

    Update playground.js

--- src/theme/playground.js
+++ src/theme/playground.js
@@ -1,13 +1,18 @@
+// $ceil[]
 function updateOutput(inputValue) {
-    const playOutput = document.getElementById('play-output');
+  const playOutput = document.getElementById('play-output');
 
-    if (!isNaN(inputValue) && inputValue !== "") {
-      const result = Math.ceil(inputValue);
-      playOutput.textContent = `Result: ${result}`;
+  if (!isNaN(inputValue) && inputValue !== "") {
+    const result = Math.ceil(inputValue);
+    playOutput.textContent = `Result: ${result}`;
+  } else {
+    if (inputValue === "") {
+      playOutput.textContent = `❌ Function $ceil at 1:1 returned an error: expected valid value in position 1, got empty value`;
     } else {
       let nonNumericIndex = inputValue.search(/[^0-9\.]/); 
-      nonNumericIndex = nonNumericIndex === -1 ? inputValue.length : nonNumericIndex;
-        
+      nonNumericIndex = nonNumericIndex === -1 ? inputValue.length : nonNumericIndex + 8;
+
       playOutput.textContent = `❌ Function $ceil at 1:${nonNumericIndex} returned an error: expected integer in position 1, got '${inputValue}'`;
     }
+  }
 }
